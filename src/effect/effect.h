99779d6d6a0d6db1a5eaa2a97da244d3d2bc5111
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2026-02-14 01:26:23 +0200

    effect: Add AnimationClock
    The AnimationClock provides a convenient way to compute animation delta
    times.

--- src/effect/effect.h
+++ src/effect/effect.h
@@ -699,12 +699,8 @@ public:
      * \li do various housekeeping tasks such as initing your effect's variables
      *      for the upcoming paint pass or updating animation's progress
      * \endlist
-     *
-     * \a presentTime specifies the expected monotonic time when the rendered frame
-     * will be displayed on the screen.
      */
-    virtual void prePaintScreen(ScreenPrePaintData &data,
-                                std::chrono::milliseconds presentTime);
+    virtual void prePaintScreen(ScreenPrePaintData &data);
     /*!
      * In this method you can:
      * \list
@@ -746,12 +742,8 @@ public:
      *
      * In OpenGL based compositing, the frameworks ensures that the context is current
      * when this method is invoked.
-     *
-     * \a presentTime specifies the expected monotonic time when the rendered frame
-     * will be displayed on the screen.
      */
-    virtual void prePaintWindow(RenderView *view, EffectWindow *w, WindowPrePaintData &data,
-                                std::chrono::milliseconds presentTime);
+    virtual void prePaintWindow(RenderView *view, EffectWindow *w, WindowPrePaintData &data);
     /*!
      * This is the main method for painting windows.
      *
--- src/scene/workspacescene.cpp
+++ src/scene/workspacescene.cpp
@@ -531,14 +531,6 @@ void WorkspaceScene::prePaint(SceneView *delegate)
 
     createStackingOrder();
 
-    const RenderLoop *renderLoop = painted_screen->backendOutput()->renderLoop();
-    const std::chrono::milliseconds presentTime =
-        std::chrono::duration_cast<std::chrono::milliseconds>(renderLoop->nextPresentationTimestamp());
-
-    if (presentTime > m_expectedPresentTimestamp) {
-        m_expectedPresentTimestamp = presentTime;
-    }
-
     // preparation step
     effects->startPaint();
 
@@ -550,7 +542,7 @@ void WorkspaceScene::prePaint(SceneView *delegate)
     effects->makeOpenGLContextCurrent();
     Q_EMIT preFrameRender();
 
-    effects->prePaintScreen(prePaintData, m_expectedPresentTimestamp);
+    effects->prePaintScreen(prePaintData);
     m_paintContext.deviceDamage = painted_delegate->mapToDeviceCoordinatesAligned(prePaintData.paint) & painted_delegate->deviceRect();
     m_paintContext.mask = prePaintData.mask;
     m_paintContext.phase2Data.clear();
@@ -595,7 +587,7 @@ void WorkspaceScene::preparePaintGenericScreen()
         data.mask = m_paintContext.mask;
         data.devicePaint = Region::infinite(); // no clipping, so doesn't really matter
 
-        effects->prePaintWindow(painted_delegate, windowItem->effectWindow(), data, m_expectedPresentTimestamp);
+        effects->prePaintWindow(painted_delegate, windowItem->effectWindow(), data);
         m_paintContext.phase2Data.append(Phase2Data{
             .item = windowItem,
             .deviceRegion = Region::infinite(),
@@ -634,7 +626,7 @@ void WorkspaceScene::preparePaintSimpleScreen()
             addOpaqueRegionRecursive(painted_delegate, windowItem, std::nullopt, data.deviceOpaque);
         }
 
-        effects->prePaintWindow(painted_delegate, windowItem->effectWindow(), data, m_expectedPresentTimestamp);
+        effects->prePaintWindow(painted_delegate, windowItem->effectWindow(), data);
         m_paintContext.phase2Data.append(Phase2Data{
             .item = windowItem,
             .deviceRegion = data.devicePaint & painted_delegate->deviceRect(),
